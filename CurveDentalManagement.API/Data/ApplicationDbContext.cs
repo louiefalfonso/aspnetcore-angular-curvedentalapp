3b0b459e285ab21bae053d264bd1cdca386e7538
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-18 20:34:37 +0100

    Created DbContext & Run EF Migrations

---
+++ CurveDentalManagement.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CurveDentalManagement.API.Model.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurveDentalManagement.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+        public DbSet<Staff> Staffs { get; set; }
+    }
+}
---
+++ CurveDentalManagement.API/Model/DTO/StaffDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CurveDentalManagement.API.Model.DTO
+{
+    public class StaffDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StaffRole { get; set; }
+        public string Email { get; set; }
+        public string Sex { get; set; }
+        public string Phone { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+    }
+}
