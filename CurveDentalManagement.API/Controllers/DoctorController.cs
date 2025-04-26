ffd45c2b00665f061efceef8cede4e659a608d74
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-26 19:57:15 +0100

    Update API Methods & Run EF Migrations for Doctors

--- CurveDentalManagement.API/Controllers/DoctorController.cs
+++ CurveDentalManagement.API/Controllers/DoctorController.cs
@@ -1,5 +1,6 @@
 ﻿using CurveDentalManagement.API.Models.Domain;
 using CurveDentalManagement.API.Models.DTO;
+using CurveDentalManagement.API.Repositories.Implementation;
 using CurveDentalManagement.API.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -60,5 +61,182 @@ namespace CurveDentalManagement.API.Controllers
 
             return Ok(response);
         }
+
+        // Get Doctor By ID
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetDoctorByIdAsync([FromRoute] Guid id)
+        {
+            // get doctor by Id at repository
+            var doctor = await doctorRepository.GetByIdAsync(id);
+
+            // check if doctor is null
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to dto
+            var response = new DoctorDto
+            {
+                Id = doctor.Id,
+                FirstName = doctor.FirstName,
+                LastName = doctor.LastName,
+                Email = doctor.Email,
+                ContactNumber = doctor.ContactNumber,
+                Specialization = doctor.Specialization,
+                Department = doctor.Department,
+                Schedule = doctor.Schedule,
+                LicenseNumber = doctor.LicenseNumber,
+                YearsOfExperience = doctor.YearsOfExperience,
+                DentalSchool = doctor.DentalSchool,
+                OfficeAddress = doctor.OfficeAddress,
+                EmergencyContact = doctor.EmergencyContact,
+            };
+
+            return Ok(response);
+        }
+
+        // Get All Doctors
+        [HttpGet]
+        public async Task<IActionResult> GetAllDoctorsAsync
+            (
+                // add filtering, sorting & pagination
+                [FromQuery] string? query,
+                [FromQuery] string? sortBy,
+                [FromQuery] string? sortDirection,
+                [FromQuery] int? pageNumber,
+                [FromQuery] int? pageSize
+            )
+        {
+            //get all doctors from rtepository
+            var doctors = await doctorRepository.GetAllAsync(query, sortBy, sortDirection, pageNumber, pageSize);
+
+            // map domain model to dto
+            var response = new List<DoctorDto>();
+            foreach (var doctor in doctors)
+            {
+                response.Add(new DoctorDto
+                {
+                    Id = doctor.Id,
+                    FirstName = doctor.FirstName,
+                    LastName = doctor.LastName,
+                    Email = doctor.Email,
+                    ContactNumber = doctor.ContactNumber,
+                    Specialization = doctor.Specialization,
+                    Department = doctor.Department,
+                    Schedule = doctor.Schedule,
+                    LicenseNumber = doctor.LicenseNumber,
+                    YearsOfExperience = doctor.YearsOfExperience,
+                    DentalSchool = doctor.DentalSchool,
+                    OfficeAddress = doctor.OfficeAddress,
+                    EmergencyContact = doctor.EmergencyContact,
+                });
+            }
+            return Ok( response );
+        }
+
+        // Update Doctor
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateDoctorAsync([FromRoute] Guid id, UpdateDoctorRequestDto request)
+        {
+            // check if doctor is null
+            if ( request == null)
+            {
+                return BadRequest();
+;           }
+
+            // convert dto to domain model
+            var doctor = new Doctor
+            {
+                Id = id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                ContactNumber = request.ContactNumber,
+                Specialization = request.Specialization,
+                Department = request.Department,
+                Schedule = request.Schedule,
+                LicenseNumber = request.LicenseNumber,
+                YearsOfExperience = request.YearsOfExperience,
+                DentalSchool = request.DentalSchool,
+                OfficeAddress = request.OfficeAddress,
+                EmergencyContact = request.EmergencyContact,
+            };
+
+            // update doctor in repository
+            doctor = await doctorRepository.UpdateAsync(doctor);
+
+            // check if staff is null
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to dto
+            var response = new DoctorDto
+            {
+                Id = doctor.Id,
+                FirstName = doctor.FirstName,
+                LastName = doctor.LastName,
+                Email = doctor.Email,
+                ContactNumber = doctor.ContactNumber,
+                Specialization = doctor.Specialization,
+                Department = doctor.Department,
+                Schedule = doctor.Schedule,
+                LicenseNumber = doctor.LicenseNumber,
+                YearsOfExperience = doctor.YearsOfExperience,
+                DentalSchool = doctor.DentalSchool,
+                OfficeAddress = doctor.OfficeAddress,
+                EmergencyContact = doctor.EmergencyContact,
+            };
+
+            return Ok(response);
+        }
+
+        // Delete Doctor
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteDoctorAsync([FromRoute] Guid id)
+        {
+            // delet doctor from repository
+            var doctor = await doctorRepository.DeleteAsync(id);
+
+            // check if doctor is null
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to dto
+            var response = new DoctorDto
+            {
+                Id = doctor.Id,
+                FirstName = doctor.FirstName,
+                LastName = doctor.LastName,
+                Email = doctor.Email,
+                ContactNumber = doctor.ContactNumber,
+                Specialization = doctor.Specialization,
+                Department = doctor.Department,
+                Schedule = doctor.Schedule,
+                LicenseNumber = doctor.LicenseNumber,
+                YearsOfExperience = doctor.YearsOfExperience,
+                DentalSchool = doctor.DentalSchool,
+                OfficeAddress = doctor.OfficeAddress,
+                EmergencyContact = doctor.EmergencyContact,
+            };
+            return Ok(response);
+        }
+
+        // Get Count
+        [HttpGet]
+        [Route("count")]
+        public async Task<IActionResult> GetDoctorsTotal()
+        {
+            var count = await doctorRepository.GetCount();
+            return Ok(count);
+        }
     }
 }
+
--- CurveDentalManagement.API/Repositories/Implementation/DoctorRepository.cs
+++ CurveDentalManagement.API/Repositories/Implementation/DoctorRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CurveDentalManagement.API.Data;
 using CurveDentalManagement.API.Models.Domain;
 using CurveDentalManagement.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace CurveDentalManagement.API.Repositories.Implementation
 {
@@ -19,6 +20,98 @@ namespace CurveDentalManagement.API.Repositories.Implementation
             await dbContext.Doctors.AddAsync(doctor);
             await dbContext.SaveChangesAsync();
             return doctor;
+        }
+
+        // Get Doctor by Id
+        public async Task<Doctor?> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Doctors.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        // Get All Doctors
+        public async Task<IEnumerable<Doctor>> GetAllAsync
+            (
+                // add filtering, sorting & pagination
+                string? query = null,
+                string? sortBy = null,
+                string? sortDirection = null,
+                int? pageNumber = 1,
+                int? pageSize = 100
+            ) 
+        { 
+            // query
+            var doctors = dbContext.Doctors.AsQueryable();
+
+            // filter
+            if (string.IsNullOrWhiteSpace(query) == false)
+            {
+                doctors = doctors.Where(
+                    x => x.FirstName.Contains(query) ||
+                    x.LastName.Contains(query) ||
+                    x.Specialization.Contains(query)
+                );
+            }
+
+            // sorting
+            if (string.IsNullOrWhiteSpace(sortBy) == false)
+            {
+                if (string.Equals(sortBy, "FirstName", StringComparison.OrdinalIgnoreCase))
+                {
+                    var isAsc = string.Equals(sortDirection, "asc", StringComparison.OrdinalIgnoreCase) ? true : false;
+                    doctors = isAsc ? doctors.OrderBy(x => x.FirstName) : doctors.OrderByDescending(x => x.FirstName);
+                }
+                if (string.Equals(sortBy, "LasttName", StringComparison.OrdinalIgnoreCase))
+                {
+                    var isAsc = string.Equals(sortDirection, "asc", StringComparison.OrdinalIgnoreCase) ? true : false;
+                    doctors = isAsc ? doctors.OrderBy(x => x.LastName) : doctors.OrderByDescending(x => x.LastName);
+                }
+                if (string.Equals(sortBy, "Specialization", StringComparison.OrdinalIgnoreCase))
+                {
+                    var isAsc = string.Equals(sortDirection, "asc", StringComparison.OrdinalIgnoreCase) ? true : false;
+                    doctors = isAsc ? doctors.OrderBy(x => x.Specialization) : doctors.OrderByDescending(x => x.Specialization);
+                }
+            }
+
+            // pagination
+            var skipResults = (pageNumber - 1) * pageSize;
+            doctors = doctors.Skip(skipResults ?? 0).Take(pageSize ?? 100);
+
+            return await doctors.ToListAsync();
+        }
+
+        // Update Doctor
+        public async Task<Doctor?> UpdateAsync(Doctor doctor)
+        {
+            var existingDoctor = dbContext.Doctors.FirstOrDefault( x => x.Id == doctor.Id);
+
+            if (existingDoctor != null) 
+            {
+                dbContext.Entry(existingDoctor).CurrentValues.SetValues(doctor);
+                await dbContext.SaveChangesAsync();
+                return doctor;
+            }
+            return null;
+        }
+
+        // Delete Doctor
+        public async Task<Doctor?> DeleteAsync(Guid id)
+        {
+            var existingDoctor = await dbContext.Doctors.FirstOrDefaultAsync( x => x.Id == id);
+
+            if (existingDoctor is null) 
+            {
+                return null;
+            }
+
+            dbContext.Doctors.Remove(existingDoctor);
+            await dbContext.SaveChangesAsync();
+            return existingDoctor;
+        }
+
+        // Get Total Count
+        public async Task<int> GetCount()
+        {
+            return await dbContext.Doctors.CountAsync();
         }
     }
 }
