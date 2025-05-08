4edd529488e69ca73e57dc25ecc4a652dd3fb758
Louie Alfonso <lfriasalfonso@gmail.com>
2025-05-08 21:35:51 +0100

    Update API Methods for Treatments

--- CurveDentalManagement.API/Controllers/TreatmentController.cs
+++ CurveDentalManagement.API/Controllers/TreatmentController.cs
@@ -200,5 +200,131 @@ namespace CurveDentalManagement.API.Controllers
             return Ok(response);
         }
 
+
+        // Update Treatment
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateTreatmentAsync([FromRoute] Guid id, UpdateTreatmentRequestDto request)
+        {
+            // map dto to domain model
+            var treatment = new Treatment
+            {
+                TreatmentName = request.TreatmentName,
+                TreatmentCode = request.TreatmentCode,
+                Description = request.Description,
+                Cost = request.Cost,
+                InsuranceCoverage = request.InsuranceCoverage,
+                InsuranceCoverageAmount = request.InsuranceCoverageAmount,
+                FollowUpCare = request.FollowUpCare,
+                RiskBenefits = request.RiskBenefits,
+                Indications = request.Indications,
+                Doctors = new List<Doctor>(),
+
+            };
+
+            // add doctor to treatment
+            foreach (var doctorGuid in request.Doctors)
+            {
+                // Get doctor By ID from Doctor Repository
+                var exisitingDoctor = await doctorRepository.GetByIdAsync(doctorGuid);
+
+                // Check if Doctor Exists
+                if (exisitingDoctor is not null)
+                {
+                    treatment.Doctors.Add(exisitingDoctor);
+                }
+            }
+
+            // Call Repository to update Prescription Domain Model
+            var updatedTreatment = await treatmentRepository.UpdateAsync(treatment);
+
+            // check if updated treatment is Null
+            if (updatedTreatment == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to dto
+            var response = new TreatmentDto
+            {
+                Id = treatment.Id,
+                TreatmentName = treatment.TreatmentName,
+                TreatmentCode = treatment.TreatmentCode,
+                Description = treatment.Description,
+                Cost = treatment.Cost,
+                InsuranceCoverage = treatment.InsuranceCoverage,
+                InsuranceCoverageAmount = treatment.InsuranceCoverageAmount,
+                FollowUpCare = treatment.FollowUpCare,
+                RiskBenefits = treatment.RiskBenefits,
+                Indications = treatment.Indications,
+                Doctors = treatment.Doctors.Select(x => new DoctorDto
+                {
+                    Id = x.Id,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Email = x.Email,
+                    ContactNumber = x.ContactNumber,
+                    Specialization = x.Specialization,
+                    Department = x.Department,
+                    Schedule = x.Schedule,
+                    LicenseNumber = x.LicenseNumber,
+                    YearsOfExperience = x.YearsOfExperience,
+                    DentalSchool = x.DentalSchool,
+                    OfficeAddress = x.OfficeAddress,
+                    EmergencyContact = x.EmergencyContact,
+
+                }).ToList()
+            };
+
+            return Ok(response);
+
+
+        }
+
+
+        // Delete Treatment
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteTreatmentAsync([FromRoute] Guid id)
+        {
+            // delete treatment
+            var deletedTreatment = await treatmentRepository.DeleteAsync(id);
+
+            // Check if Deleted Treatment is Null
+            if (deletedTreatment == null)
+            {
+                return NotFound();
+            }
+
+            // map domain model to dto
+            var response = new TreatmentDto
+            {
+                Id = deletedTreatment.Id,
+                TreatmentName = deletedTreatment.TreatmentName,
+                TreatmentCode = deletedTreatment.TreatmentCode,
+                Description = deletedTreatment.Description,
+                Cost = deletedTreatment.Cost,
+                InsuranceCoverage = deletedTreatment.InsuranceCoverage,
+                InsuranceCoverageAmount = deletedTreatment.InsuranceCoverageAmount,
+                FollowUpCare = deletedTreatment.FollowUpCare,
+                RiskBenefits = deletedTreatment.RiskBenefits,
+                Indications = deletedTreatment.Indications,
+            };
+
+            return Ok(response);
+
+        }
+
+        // Get Count
+        [HttpGet]
+        [Route("count")]
+        public async Task<IActionResult> GetTreatmentsTotal()
+        {
+            var count = await treatmentRepository.GetCount();
+            return Ok(count);
+        }
+
+
     }
 }
+
