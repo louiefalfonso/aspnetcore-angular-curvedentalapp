a256de8dbeb86f8ba98ea5d066ff9447c03afc0c
Louie Alfonso <lfriasalfonso@gmail.com>
2025-05-16 17:14:08 +0100

    Added Appointment DB & Run EF Migrations

--- CurveDentalManagement.API/Migrations/20250516161124_Created Appointment DB with Doctors & Patients.Designer.cs
+++ CurveDentalManagement.API/Migrations/20250516161124_Created Appointment DB with Doctors & Patients.Designer.cs
@@ -4,6 +4,7 @@ using CurveDentalManagement.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CurveDentalManagement.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250516161124_Created Appointment DB with Doctors & Patients")]
+    partial class CreatedAppointmentDBwithDoctorsPatients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CurveDentalManagement.API/Migrations/20250516161124_Created Appointment DB with Doctors & Patients.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurveDentalManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedAppointmentDBwithDoctorsPatients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppointmentCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppointmentDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    AppointmentTime = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentDoctor",
+                columns: table => new
+                {
+                    AppointmentsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DoctorsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentDoctor", x => new { x.AppointmentsId, x.DoctorsId });
+                    table.ForeignKey(
+                        name: "FK_AppointmentDoctor_Appointments_AppointmentsId",
+                        column: x => x.AppointmentsId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentDoctor_Doctors_DoctorsId",
+                        column: x => x.DoctorsId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentPatient",
+                columns: table => new
+                {
+                    AppointmentsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PatientsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentPatient", x => new { x.AppointmentsId, x.PatientsId });
+                    table.ForeignKey(
+                        name: "FK_AppointmentPatient_Appointments_AppointmentsId",
+                        column: x => x.AppointmentsId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentPatient_Patients_PatientsId",
+                        column: x => x.PatientsId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentDoctor_DoctorsId",
+                table: "AppointmentDoctor",
+                column: "DoctorsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentPatient_PatientsId",
+                table: "AppointmentPatient",
+                column: "PatientsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentDoctor");
+
+            migrationBuilder.DropTable(
+                name: "AppointmentPatient");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
