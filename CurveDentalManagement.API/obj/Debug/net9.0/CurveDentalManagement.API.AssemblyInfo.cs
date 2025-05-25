5dc3c4ccc3ad67a2affc230c43b0d7a4d13c1c0d
Louie Alfonso <lfriasalfonso@gmail.com>
2025-05-25 19:28:39 +0100

    Update Curve Dental Management API DbContext

--- CurveDentalManagement.API/obj/Debug/net9.0/CurveDentalManagement.API.AssemblyInfo.cs
+++ CurveDentalManagement.API/obj/Debug/net9.0/CurveDentalManagement.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurveDentalManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e835d7bfddcacd42e5d6f97990d0e2e43b59412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76af2173ad2f7d580babb0412a9e39071d8cc37")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurveDentalManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurveDentalManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
